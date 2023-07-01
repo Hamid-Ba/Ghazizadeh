b013d94746d55f145ddac39425c7383652a9ebe7
hamidba <balalzadehhamid79@gmail.com>
2023-07-01 11:31:42 +0330

    complete province module

---
+++ address/tests/tests_model.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+# Create your tests here.
+# class AddressModel(TestCase):
+#     """Address Model Test"""
+#     def test_address_model(self):
+#         """Test Address Model"""
+#         payload = {
+#             "province" : "province"
+#         }
--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     "siteinfo",
     "gallery",
     "brand",
+    "province"
 ]
 
 MIDDLEWARE = [
