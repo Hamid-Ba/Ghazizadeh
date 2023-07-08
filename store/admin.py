afc581709ba417eab241b20e424ae4feda38f3a8
hamidba <balalzadehhamid79@gmail.com>
2023-07-09 01:11:14 +0330

    update store category admin model

--- store/admin.py
+++ store/admin.py
@@ -3,10 +3,38 @@ from django.contrib import admin
 from store import models
 
 
+class SubCategoryInline(admin.StackedInline):
+    model = models.Category
+    extra = 0
+    verbose_name_plural = "Sub Categories"
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "order", "parent")
+    list_filter = ("parent",)
+    search_fields = ("title",)
+
+    inlines = [SubCategoryInline]
+
+    fieldsets = (
+        (None, {"fields": ("title", "logo", "order")}),
+        (
+            "Parent",
+            {
+                "fields": ("parent",),
+                "description": "Select a category to make it a parent category",
+            },
+        ),
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.prefetch_related("sub_categories")
+
+        return queryset
+
 class SpecificationInline(admin.TabularInline):
     model = models.Specifications
     extra = 1
-
 
 class ProductAdmin(admin.ModelAdmin):
     # list_display = ("title", "category", "publish_date")
@@ -50,5 +78,5 @@ class ProductAdmin(admin.ModelAdmin):
     #     return queryset
 
 
-admin.site.register(models.Category)
+admin.site.register(models.Category , CategoryAdmin)
 admin.site.register(models.Product, ProductAdmin)
