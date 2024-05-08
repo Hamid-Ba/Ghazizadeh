9a55dc077463093a3dd4df1d158602aa2c3edd5b
hamidba <balalzadehhamid79@gmail.com>
2024-05-08 16:14:36 +0330

    add image path to gallery

--- gallery/admin.py
+++ gallery/admin.py
@@ -8,10 +8,12 @@ from gallery import models
 class GalleryAdmin(admin.ModelAdmin):
     """Gallery Admin Model"""
 
-    list_display = ["id", "title"]
+    list_display = ["id", "title", "image_path"]
     list_display_links = ["id", "title"]
     sortable_by = ["title"]
     search_fields = ["title"]
 
+    def image_path(self, obj):
+        return obj.image.path
 
 admin.site.register(models.Gallery, GalleryAdmin)
